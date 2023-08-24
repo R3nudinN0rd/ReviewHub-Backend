77e292fbefbbcefba085e6007ad6f9ee499c4e1b
R3nudinN0rd <remusene69@gmail.com>
2023-08-24 15:25:46 +0300

    added logger service

---
+++ LoggerService/Entities/LogAction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggerService.Entities
+{
+    public class LogAction
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Action code is required!")]
+        public int ActionCode { get; set; }
+        [Required(ErrorMessage = "Action message!")]
+        public string ActionMessage { get; set; }
+        public DateTime ActionDate { get; set; }
+    }
+}
---
+++ LoggerService/Entities/LogError.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoggerService.Entities
+{
+    public class LogError
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Error code is required!")]
+        public int ErrorCode { get; set; }
+        [Required(ErrorMessage = "Error message is required!")]
+        public string ErrorMessage { get; set; }
+        public DateTime LoggedDate { get; set; }
+    }
+}
